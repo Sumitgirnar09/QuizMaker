15bb0bdef9eda9d67b6d8db0679912e8306a2623
Sumit Sunil Girnar <sumitgirnar09@gmail.com>
2022-10-26 14:15:44 +0530

    Created Admin Template , Admin Login , administrator app

--- administrator/models.py
+++ administrator/models.py
@@ -1,3 +1,17 @@
+from ast import Pass
+from asyncio.windows_events import NULL
+from email.policy import default
 from django.db import models
 
 # Create your models here.
+
+class Administrator(models.Model):
+    
+    Name=models.CharField(max_length=200,default=NULL)
+    Username=models.CharField(max_length=200,default=NULL)
+    Email=models.EmailField(max_length=200)
+    Pass=models.CharField(max_length=200)
+    
+    
+    
+    
